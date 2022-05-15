77e71e149fab773ad407c4372ac9b7b6c7cbfb18
Yutthana00 <arf101293@gmail.com>
2022-05-15 15:45:56 +0100

    remove, update, adding opinion

--- controllers/opinionController.js
+++ controllers/opinionController.js
@@ -2,7 +2,7 @@ const  { Opinion, User } = require('../models');
 const { route } = require('../routes/api/opinionRoutes');
 
 module.exports = {
-    
+
     // Get opinion by ID:
     getOpinionByID({ params }, res) {
         Opinion.findOne({ _id: params.opinionId })
@@ -29,6 +29,72 @@ module.exports = {
             });
     },
 
+    // Update opinion by ID:
+    updateOpinion({ params, body }, res) {
+        Opinion.findOneAndUpdate({ _id: params.thoughtId }, body, {
+            new: true,
+            runValidators: true,
+        })
+        .then((opinionData) => {
+            if (!opinionData) {
+                res.status(404).JSON({ message: "No comment with ID"});
+                return;
+            }
+            res.JSON(opinionData);
+        })
+        .catch((err) => res.status(400).JSON(err));
+    },
 
+    // Adding an opinion to a user:
+    addOpinion({ params, body }, res) {
+        console.log(body);
+        Opinion.create(body)
+            .then(({ _id }) => {
+                return User.findOneAndUpdate(
+                    { _id: params.userId },
+                    { $push: { opinions: _id }},
+                    { new: true }
+                );
+            })
+            .then((userData) => {
+                if (!userData) {
+                    res.status(404).JSON({ message: "USER ID NOT FOUND"});
+                    return;
+                }
+                res.JSON(userData);
+            })
+            .catch((err) => res.JSON(err));
+    },
+    
+    // Remove opinion:
+    removeOpinion({ params }, res) {
+        Opinion.findOneAndDelete({ _id:params.opinionId })
+        .then((deleteOpinion) => {
+            if (!deleteOpinion) {
+                return res.status(404).JSON({ message: "Comment not found with this ID"});
+            }
+            res.JSON(deleteOpinion);
+        })
+        .catch((err) => res.status(400).JSON(err));
+    },
 
+    // Update opinion by ID:
+    updateOpinion({ params, body }, res) {
+        Opinion.findOneAndUpdate({ _id: params.opinionId }, body, {
+            new: true,
+            runValidators:true,
+        })
+        .then((opinionData) => {
+            if (!opinionData) {
+                res.status(404).JSON({ message: "No opinion found with this ID"});
+                return;
+            }
+            res.json(opinionData);
+        })
+        .catch((err) => res.status(400).JSON(err));
+    },
+    // Add new Reaction
+    addReaction({ params, body }, res) {
+        { _id: params.}
+    }
 }
