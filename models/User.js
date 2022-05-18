dc786bea2ec5f884c75c6f94b4c16ad7640c1759
Yutthana00 <arf101293@gmail.com>
2022-05-18 03:01:27 +0100

    User model added

---
+++ models/User.js
@@ -0,0 +1,38 @@
+const { Schema, model } = require("mongoose");
+
+// Defined user schema:
+const userSchema = new Schema(
+  {
+    username: {
+      type: String,
+      required: true,
+      trim: true,
+      unique: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+      match: [/.+@.+\..+/],
+    },
+    opinions: [{ type: Schema.Types.ObjectId, ref: "opinion" }],
+    friends: [{ type: Schema.Types.ObjectId, ref: "user" }],
+  },
+  {
+    toJSON: {
+      virtuals: true,
+    },
+    id: false,
+  }
+);
+
+// Friends Count 
+userSchema.virtual("friendCount").get(function () {
+  return this.friends.length;
+});
+
+// Create user model:
+
+const User = model("user", userSchema);
+
+module.exports = User;
