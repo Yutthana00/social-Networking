c977c567669fd8e45ad0eb7a312491a0e763515f
Yutthana00 <arf101293@gmail.com>
2022-06-03 03:11:44 +0100

    app routes fixed

--- server.js
+++ server.js
@@ -10,21 +10,15 @@ const PORT = process.env.PORT || 3001;
 // Middle ware:
 app.use(express.json());
 app.use(express.urlencoded({ extended: true}));
+app.use(routes);
 
-app.use('./routes');
 
 //Connection to MongoDB database:
 mongoose.connect(
-    process.env.MONGODB_URL || "mongodb://127.0.0.1:27017/social-linkup-api",
-    {
-        useNewUrlParser: true,
-        // checking network connection
-        useUnifiedTopology: true,
-    }
-);
+    process.env.MONGODB_URL || 'mongodb://localhost:27017/social-linkup-api', {});
 
 // Log mongo:
 mongoose.set('debug, true');
 
 // Server listening response:
-app.listen(PORT, () => console.log('Connection is live on:${PORT}'));
+app.listen(PORT, () => console.log('Good connection on:${PORT}'));
