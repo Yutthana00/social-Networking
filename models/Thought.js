dd8b2e2604ba046005e0eb0f893cc86aa074128e
Yutthana00 <arf101293@gmail.com>
2022-06-03 03:45:23 +0100

    removed unused code and debugging

--- models/Thought.js
+++ models/Thought.js
@@ -1,29 +1,42 @@
-const { Schema, model } = require("mongoose");
-const moment = require("moment");
-const reactionSchema = require("./Reaction");
+const { Schema, model } = require('mongoose');
+const moment = require('moment');
+const reactionSchema = require('./Reaction');
 
-const thoughtSchema = new Schema({
-  ThoughtText: {
-    type: String,
-    required: true,
-    len: [1, 280],
+const thoughtSchema = new Schema(
+  {
+    ThoughtText: {
+      type: String,
+      required: true,
+      minLength: 1,
+      maxLength: 280,
+    },
+    createdAt: {
+      type: Date,
+      default: Date.now,
+      get: (createdAt) =>
+        moment(createdAt).format('MMM DD, YYYY [at] hh:mm a'),
+    },
+    username: {
+      type: String,
+      required: true,
+      ref: "User",
+    },
+    reactions: [reactionSchema],
   },
-  createdAt: {
-    type: Date,
-    required: true,
-    default: Date.now,
-    get: (createdAtVal) =>
-      moment(createdAtVal).format("MMM DD, YYYY [at] hh:mm a"),
-  },
-  username: {
-    type: String,
-    required: true,
-    ref: "user",
-  },
-  reactions: [reactionSchema],
+  {
+    toJSON: {
+      virtuals: true,
+      getters: true,
+    },
+    id: false,
+  }
+);
+
+thoughtSchema.virtual('reactionCount').get(function() {
+  return this.reactions.length;
 });
 
-const Thought = model("thought", thoughtSchema);
+const Thought = model('Thought', thoughtSchema);
 
 module.exports = Thought;
 
