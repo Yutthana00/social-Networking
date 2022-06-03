41a5d079a8db3018d146aae05b69f174b4430c32
Yutthana00 <arf101293@gmail.com>
2022-06-03 04:06:51 +0100

    added comments

--- routes/api/userRoutes.js
+++ routes/api/userRoutes.js
@@ -8,11 +8,11 @@ const {
     addFriend, 
     deleteFriend
 } = require('../../controllers/usersController');
-
+// Call all users, create new users:
 router.route('/').get(getAllUsers).post(createUser);
-
+// Get user by id, edit user and delete user:
 router.route('/:id').get(getUserById).put(editUser).delete(deleteUser);
-
+// Add and delete friends:
 router.route('/:userId/friends/:friendId').post(addFriend).delete(deleteFriend);
 
 module.exports = router;
