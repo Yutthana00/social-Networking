20d6a5c91b32230b21f83eb62201be9967f89cdf
Yutthana00 <arf101293@gmail.com>
2022-06-03 04:05:23 +0100

    layed out new get, post, put, dele routes

--- routes/api/userRoutes.js
+++ routes/api/userRoutes.js
@@ -9,6 +9,10 @@ const {
     deleteFriend
 } = require('../../controllers/usersController');
 
+router.route('/').get(getAllUsers).post(createUser);
 
+router.route('/:id').get(getUserById).put(editUser).delete(deleteUser);
+
+router.route('/:userId/friends/:friendId').post(addFriend).delete(deleteFriend);
 
 module.exports = router;
