d5bc278446f6aa5c6485ab2c69ac1523a501687a
Yutthana00 <arf101293@gmail.com>
2022-06-30 21:27:17 +0100

    name changes

--- routes/api/userRoutes.js
+++ routes/api/userRoutes.js
@@ -1,9 +1,10 @@
 const router = require('express').Router();
+
 const {
     getAllUsers, 
     getUserById, 
     createUser, 
-    editUser, 
+    updateUser, 
     deleteUser, 
     addFriend, 
     deleteFriend
@@ -11,8 +12,11 @@ const {
 // Call all users, create new users:
 router.route('/').get(getAllUsers).post(createUser);
 // Get user by id, edit user and delete user:
-router.route('/:id').get(getUserById).put(editUser).delete(deleteUser);
+router.route("/:id")
+.get(getUserById)
+// .put(updateUser)
+.delete(deleteUser);
 // Add and delete friends:
-router.route('/:userId/friends/:friendId').post(addFriend).delete(deleteFriend);
+router.route('/:id/friends/:friendId').post(addFriend).delete(deleteFriend);
 
 module.exports = router;
